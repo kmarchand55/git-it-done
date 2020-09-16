f2c721c9fe8b15b95ce28dd0526abca9b354d66e
Ken Marchand <marchand_kenneth@smc.edu>
2020-09-16 13:56:32 -0700

    Added fetch procedures

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,24 +4,31 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var getUserRepos = function (user) {
-  var apiUrl = "https://api.github.com/users/" + user + "/respos";
-  fetch(apiUrl).then(function (response) {
+  // format the github api url
+  //var apiUrl = "https://api.github.com/users/" + user + "/repos";
+var apiUrl = "http://api.weatherstack.com/current?access_key=be4f9fcc21f5ff497287420df66d5c79&query=Santa%20Monica,%20CA"
+  // make a request to the url
+  fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
     if (response.ok) {
-      response.json().then(function (data) {
+      response.json().then(function(data) {
         displayRepos(data, user);
       });
     } else {
-      alert("Error:" + response.statusText);
+      alert("Error: " + response.statusText);
     }
-    
   })
-  .catch(function(error){
-      alert("Unable to connecto to Github")
-  
-});
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
+  });
+};
 
-formSubmitHandler = function (event) {
+var formSubmitHandler = function (event) {
   event.preventDefault();
+  console.log(event);
+  // get value from input element
   var username = nameInputEl.value.trim();
 
   if (username) {
@@ -30,18 +37,23 @@ formSubmitHandler = function (event) {
   } else {
     alert("Please enter a GitHub username");
   }
-  console.log(event);
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 
 var displayRepos = function (repos, searchTerm) {
-  //console.log("This is " + repos);
-  //console.log("This is " + searchTerm);
+    // check if api returned any repos
+if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
+  console.log(repos);
+  console.log(searchTerm);
   // clear old content
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
   // loop over repos
+
   for (var i = 0; i < repos.length; i++) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
@@ -56,7 +68,6 @@ var displayRepos = function (repos, searchTerm) {
 
     // append to container
     repoEl.appendChild(titleEl);
-
     // create a status element
     var statusEl = document.createElement("span");
     statusEl.classList = "flex-row align-center";
@@ -74,14 +85,7 @@ var displayRepos = function (repos, searchTerm) {
 
     // append to container
     repoEl.appendChild(statusEl);
-
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
-
-    // check if api returned any repos
-    if (repos.length === 0) {
-      repoContainerEl.textContent = "No repositories found.";
-      return;
-    }
   }
 };
---
+++ assets/js/single.js
@@ -0,0 +1,7 @@
+var getRepoIssues = function(repo) {
+    console.log(repo);
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl);
+  };
+  
+  getRepoIssues("facebook/react");
